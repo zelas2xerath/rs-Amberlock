47767acff117afa0ea2309cbaf74545ecf67eea9
zelas2xerath <leancode@189.cn>
2025-12-08 22:10:54 +0800

    Fix

--- amberlock-core/src/inspect.rs
+++ amberlock-core/src/inspect.rs
@@ -1,14 +1,15 @@
 use amberlock_winsec as winsec;
+use amberlock_types::Capability;
 
 #[derive(Debug, Clone)]
 pub struct InspectReport {
-    pub capability: amberlock_types::Capability,
+    pub capability: Capability,
 }
 
 pub fn probe_capability() -> anyhow::Result<InspectReport> {
     let p = winsec::token::probe_capability()?;
     Ok(InspectReport {
-        capability: amberlock_types::Capability {
+        capability: Capability {
             caller_il: p.caller_il,
             can_touch_sacl: p.has_se_security,
             can_set_system: p.has_se_relabel,
